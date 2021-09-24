30e743710dfb3336a911f130574a98cadb106ded
OzDV696 <OzDV696@users.noreply.github.com>
2021-09-23 23:19:53 -0500

    Agregar archivos de proyecto.

---
+++ App3/App3/ViewModels/NombreCompletoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace App3.ViewModels
+{
+    public class NombreCompletoViewModel : BaseViewModel
+    {
+        private string _nombre;
+        private string _paterno;
+        private string _materno;
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+                _nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(Completo));
+            }
+        }
+        public string Paterno
+        {
+            get => _paterno;
+            set
+            {
+                if (string.Equals(_paterno, value)) return;
+                _paterno = value;
+                OnPropertyChanged(nameof(Paterno));
+                OnPropertyChanged(nameof(Completo));
+            }
+        }
+        public string Materno
+        {
+            get => _materno;
+            set
+            {
+                if (string.Equals(_materno, value)) return;
+                _materno = value;
+                OnPropertyChanged(nameof(Materno));
+                OnPropertyChanged(nameof(Completo));
+            }
+        }
+
+        public string Completo => $"{Nombre} {Paterno} {Materno}";
+    }
+}
